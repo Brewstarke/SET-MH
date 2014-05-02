03489991242723e5136ee1b70e8ebaf1ee15cdee
Brewstarke <afstarke@gmail.com>
2014-05-02 16:40:27 -0400

    Update table

--- src/Summaries.R
+++ src/Summaries.R
@@ -4,18 +4,21 @@ require(reshape2)
 require(plyr)
 require(dplyr)
 
-SummaryTable <- join(SA.site.means, SET.site.means, by= c("Stratafication", "Site_Name", "SET_Type"))
+SummaryTable <- join(y= SA.site.means,  
+		     x= SET.site.means, 
+		     by= c("Stratafication", "Site_Name"), 
+		     type= "left")
 
 SummaryTable$SubSurface_change <- SummaryTable$Mean_elevation_change - SummaryTable$Mean_Accretion_Rate
 
 SiteVisits <- SET.data %.%
-									group_by( Site_Name, Stratafication ) %.%
-									summarise("Sample N" = length(unique(Start_Date)))
+		group_by( Site_Name, Stratafication, SET_Type ) %.%
+		summarise("Sample N" = length(unique(Start_Date)))
 
-SummaryTable <- join(SiteVisits, SummaryTable, by= c("Stratafication", "Site_Name"))
+SummaryTable <- join(x= SiteVisits, y= SummaryTable, by= c("Stratafication", "Site_Name", "SET_Type"))
 
 SummaryTableCoords <- join(x= SummaryTable, y= StudySites, by= "Site_Name", type= "left", match= 'first')
-SummaryTableCoords <- SummaryTableCoords[,c(1:8, 20:21, 37)]
+SummaryTableCoords <- SummaryTableCoords[,c(1:9,18, 21:26)]
 
 write.xlsx(x= SummaryTableCoords, file="reports/SummaryTable.xls", append= FALSE)
 
