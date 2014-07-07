021e5e78929b257c0f76bac7dccb09e9b8bef526
Adam Starke <afstarke@gmail.com>
2014-07-07 17:27:13 -0400

    New plot combining the SET and SA data into a SET-MH representative plot- Note: these data are only representative and not statistically summarized as suggested. The plots take raw data and plots it against a general linear model (assumes independance across all points, which is not the appropriate method)

---
+++ graphs/SET-MH_Plots.R
@@ -0,0 +1,70 @@
+
+
+SET_Plot_overlayvert <- function(title)
+{
+	require(ggplot2)
+	require(ggthemes)
+	require(plyr)
+	require(reshape2)
+	require(scales)
+	
+	title <- title
+	data <- SET.data.M[(SET.data.M$SET_Type == "Rod SET"),] # Subset the data type - removed to add faceting on this variable
+	#data <- join(x = SET.data.M, y= SA.data.M, by = c("Site_Name", "Stratafication", "Plot_Name", "Start_Date"))
+	data_01 <- SA.data.M
+	data_01 <- rename(x = data_01, replace = c("Start_Date" = "Date", "Accretion" = "change"))
+	SiteOrder <- unique(SET.data.M$Site_Name)[c(9,8,1:2, 4, 6, 7, 3, 5)]
+	data$Site_Name <- factor(data$Site_Name, levels= SiteOrder) 
+	
+	# Add control structure to ensure Site is available to select.
+	#data$highlight <-  as.factor(data$Site_Name != highlightSite)
+	
+	SET_Plot2 <- ggplot()
+	
+	SET_Plot2A <- SET_Plot2 + 		
+		stat_smooth(data= data, # SET data
+			    aes(y = change,
+			        x = as.Date(Date),
+			        color= interaction(Site_Name, 
+				    		   Stratafication, sep = " "),
+			        group= interaction(Site_Name, 
+				    		   Stratafication,
+				    		   sep= " ")),
+			    method = glm,
+			    se = FALSE, 
+			    size = 1,
+			    linetype= "dotted") + 
+	    	stat_smooth(data= data_01, # SA data 
+	    		    aes(y = change,
+	    		        x = as.Date(Date),
+	    		        color= interaction(Site_Name, 
+	    		        		   Stratafication, sep = " "),
+	    		        group= interaction(Site_Name,
+	    		        		   Stratafication,
+	    		        		   sep= " ")),
+	    		    linetype= "solid",
+	    		    size= 1,
+	    		    method = glm,
+	    		    se = FALSE) + 
+		
+		facet_wrap( ~ Site_Name, ncol = 3) +
+		scale_x_date(breaks = "6 months", 
+			     labels=date_format("%Y"), 
+			     limits = as.Date(c('2011-10-01','2014-10-01'))) +
+		theme_bw(base_family="serif") + 
+		theme(legend.position= "none") +
+		labs(list(title = title, 
+			  y = "Relative Elevation of marsh surface (mm)", 
+			  x = "Date"))+
+		scale_linetype_identity(name= "Monitoring Type", 
+					guide= "legend", 
+					labels = c("Overall Elevation", "Surface Accretion"))
+	
+	SET_Plot2A
+}
+
+#####
+
+##
+
+SET_Plot_overlayvert(title = "Marsh Elevation Changes Across LI")
