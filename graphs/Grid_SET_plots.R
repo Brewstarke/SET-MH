23ea197114beecf5c690922b9a9a4adaeb641f7c
Adam Starke <afstarke@gmail.com>
2014-07-07 15:11:37 -0400

    Add new plot functions altering layout

--- graphs/Grid_SET_plots.R
+++ graphs/Grid_SET_plots.R
@@ -1,6 +1,4 @@
-
-
-SET_Plot_overlayvert <- function(highlightSite = NULL, title)
+SET_Plot_grid <- function(highlightSite = NULL, title)
 {
 	require(ggplot2)
 	require(ggthemes)
@@ -9,34 +7,34 @@ SET_Plot_overlayvert <- function(highlightSite = NULL, title)
 	require(scales)
 	
 	title <- title
-# 	data <- SET.data.M[(SET.data.M$SET_Type == SET_Type),] # Subset the data type - removed to add faceting on this variable
+	# 	data <- SET.data.M[(SET.data.M$SET_Type == SET_Type),] # Subset the data type - removed to add faceting on this variable
 	data <- SET.data.M
-		SiteOrder <- unique(SET.data.M$Site_Name)[c(9,8,1:7)]
-			data$Site_Name <- factor(data$Site_Name, levels= SiteOrder) 
+	SiteOrder <- unique(SET.data.M$Site_Name)[c(9,8,1:7)]
+	data$Site_Name <- factor(data$Site_Name, levels= SiteOrder) 
 	
 	# Add control structure to ensure Site is available to select.
-		data$highlight <-  as.factor(data$Site_Name == highlightSite)
+	data$highlight <-  as.factor(data$Site_Name == highlightSite)
 	
 	SET_Plot2 <- ggplot(data= data, aes(y = change, 
-					    x = Date, color= highlight,
+					    x = Date, color= Site_Name,
 					    group= interaction(
 					    	Site_Name, 
 					    	Stratafication,
 					    	SET_Type,
 					    	sep= " "
-					    	)
-					    ))
+					    )
+	))
 	
 	
 	SET_Plot2A <- SET_Plot2 + 
-	
+		
 		stat_smooth(method = glm, size = 1,  se = FALSE) + 
 		stat_smooth(method= loess, se= FALSE, size= .5, alpha = 1) + 
 		
-		facet_grid( ~ Site_Name) +
+		facet_wrap( ~ Site_Name, ncol = 2) +
 		scale_x_date(breaks = "6 months", 
 			     labels=date_format("%Y"), 
-			     limits = as.Date(c('2011-01-01','2014-10-01'))) + 
+			     limits = as.Date(c('2011-05-01','2014-05-01'))) + 
 		
 		theme_bw(base_family="serif") + 
 		theme(legend.position= "none") +
@@ -44,7 +42,7 @@ SET_Plot_overlayvert <- function(highlightSite = NULL, title)
 		labs(list(title = title, 
 			  y = "Relative Elevation of marsh surface (mm)", 
 			  x = "Date")) 
-		
+	
 	SET_Plot2A
 }
 #####
@@ -52,8 +50,4 @@ SET_Plot_overlayvert <- function(highlightSite = NULL, title)
 ##
 
 
-SET_Plot_overlayvert(highlightSite = c("North Greensedge - West Hempstead", "Lawrence Marsh"), title = "Marsh elevation change across LI")
-
-
-SET_Plot_overlayvert(highlightSite = c("Mashomack Point", "Bass Creek"), title = "Marsh elevation changes across LI")
-
+SET_Plot_grid(highlightSite = c("North Greensedge - West Hempstead", "Lawrence Marsh"), title = "Marsh elevation change across LI")
---
+++ graphs/SA_plots_Plots.R
@@ -0,0 +1,52 @@
+SA_Plot_grid <- function(highlightSite = NULL, title)
+{
+	require(ggplot2)
+	require(ggthemes)
+	require(plyr)
+	require(reshape2)
+	require(scales)
+	
+	title <- title
+	# 	data <- SET.data.M[(SET.data.M$SET_Type == SET_Type),] # Subset the data type - removed to add faceting on this variable
+	data <- SA.data.M
+	SiteOrder <- unique(SA.data.M$Site_Name)[c(9,8,1:7)]
+	data$Site_Name <- factor(data$Site_Name, levels= SiteOrder) 
+	
+	# Add control structure to ensure Site is available to select.
+	data$highlight <-  as.factor(data$Site_Name == highlightSite)
+	
+	S <- ggplot(data= data, aes(y = Accretion, 
+					    x = as.Date(Start_Date), color= Site_Name,
+					    group= interaction(
+					    	Site_Name, 
+					    	Stratafication,
+					    	sep= " "
+					    )
+	))
+	
+	
+	SA <- S + 
+		
+		stat_smooth(method = glm, size = 1,  se = FALSE) + 
+		stat_smooth(method= loess, se= FALSE, size= .5, alpha = 1) + 
+		
+		facet_wrap( ~ Site_Name, ncol = 1) +
+		scale_x_date(breaks = "6 months", 
+			     labels=date_format("%Y"), 
+			     limits = as.Date(c('2011-10-01','2014-10-01'))) + 
+		
+		theme_bw(base_family="serif") + 
+		theme(legend.position= "none") +
+		
+		labs(list(title = title, 
+			  y = "Accretion on marsh surface (mm)", 
+			  x = "Date")) 
+	
+	SA
+}
+#####
+
+##
+
+
+SA_Plot_grid(highlightSite = c("North Greensedge - West Hempstead", "Lawrence Marsh"), title = "Marsh Accretion Across LI")
