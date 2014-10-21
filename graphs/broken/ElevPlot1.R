ee8b2b688e7057bf5b5dca7ca7564628f5a9c6ac
Adam Starke <afstarke@gmail.com>
2014-10-21 00:02:03 -0400

    revive to working

--- graphs/broken/ElevPlot1.R
+++ graphs/broken/ElevPlot1.R
@@ -10,8 +10,8 @@ elevPlot1 <- ggplot (data= SummaryTable,
                          shape= SummaryTable$bgrate, color= SummaryTable$SubSurface_change))
 
 elevPlot1 + 
-  ylim(0, 8) +
-  xlim(0, 14) +
+  ylim(0, 10) +
+  xlim(0, 10) +
   scale_colour_continuous(low= 'red', high= 'blue') +
   geom_point(size = 10, 
              alpha= .4 ) +
@@ -20,10 +20,10 @@ elevPlot1 +
   geom_abline(slope = 1, 
               linetype='longdash', 
               color=0xFEE8C8) + 
-  geom_vline(xintercept = 2.8, 
+  geom_hline(yintercept = 2.8, 
              color= "red", 
-             alpha = .2, size = 3) + 
-  geom_vline(xintercept = 2.8, 
+             alpha = .1, size = 5) + 
+  geom_hline(yintercept = 2.8, 
              color= 'red') +
   geom_text(colour= "black", 
             label= interaction(SummaryTable$Stratafication, SummaryTable$Site_Name, sep="- "), 
@@ -31,12 +31,13 @@ elevPlot1 +
             hjust= 0) +  
   labs(y = "Average change in marsh elevation (mm/year)", 
        x = "Average surface accretion (mm/year)") + 
-  annotate("text", x = 2.5, y = 5, 
+  annotate("text", x = 0.35, y = 3, 
            label = "10-yr average sea level rise trend", 
-           angle = 90) +
+           angle = 0) +
 #   annotate("text", x = 5, y= 5.1, 
 #            label ="Change in surface elevation equal to surface accretion (without compaction)", 
 #            angle = 27) +
+	annotate("rect", xmin= 0, xmax= 10, ymin= 0, ymax= 2.8, fill= "red", alpha = 0.2) +
   theme_bw(base_family="serif") +
   theme(legend.position = "none")
 
