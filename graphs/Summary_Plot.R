15c60451f1c4c193979b9d6e0d15dbc4b512a891
Adam Starke <afstarke@gmail.com>
2014-10-21 00:01:40 -0400

    add function slopeer

--- graphs/Summary_Plot.R
+++ graphs/Summary_Plot.R
@@ -4,4 +4,14 @@ library(ggvis)
 
 SET.data.M %>%
  	group_by(Site_Name, Stratafication, Plot_Name) %>%
+	summarise(change = slopeer(SET.data.M))
 	
+
+slopeer <- function(d) {
+	if(nrow(d) < 2) {
+		return(data.frame(intercept = NA, slope = NA))
+	}else {
+		p <-  coef(lm(Raw ~ DecYear, data = d))
+		p <- data.frame(slope = round(p[2], digits= 4))
+	}
+}
