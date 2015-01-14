4d4513327becff414b62c0079df1cf62219c3ce7
Adam Starke <afstarke@gmail.com>
2015-01-14 15:55:31 -0500

    fix plyr -> dplyr issue

--- diagnostics/Diagnostic_plots.R
+++ diagnostics/Diagnostic_plots.R
@@ -3,7 +3,7 @@ require(ggplot2)
 require(reshape2)
 require(plyr)
 
-SET.data.Melt <- rename(x= SET.data.M, replace= c(Raw = "value"))
+SET.data.Melt <- plyr::rename(x= SET.data.M, replace= c(Raw = "value"))
 dcast(data= SET.data.Melt, formula= Site_Name ~ SET_Type + Stratafication)
 dcast(data= SET.data.Melt, formula= Site_Name + SET_Type ~ Stratafication)
 d <- dcast(data= SET.data.Melt, formula= Site_Name ~ SET_Type, count(unique(SET.data.Melt$Position_ID)))
@@ -11,3 +11,4 @@ dcast(data= SET.data.Melt, formula= Position_ID ~ SET_Type + Stratafication)
 dcast(data= SET.data.Melt, formula= Position_ID ~ Location_ID)
 
 Shallows <- subset.data.frame(x= SET.data.M, subset= c(SET_Type == "Shallow SET"))
+Shallows
