bcbf285d052894e3d9d9438fc84a35490d9287c4
Adam Starke <afstarke@gmail.com>
2015-10-09 10:37:56 -0400

    clean out redundant functions

--- lib/helpers.R
+++ lib/helpers.R
@@ -1,33 +1,10 @@
 ### Helper Funcitons for various issues encountered with the data.
 
+
+#*************************************************************
+## Capwords ------------------------------------------------------
 # Character case issues:
 # Converting first character in each word from lower case to upper case
-
-# required.packages <- c("reshape2", 
-#                        "RODBC", 
-#                        "plyr", 
-#                        "xlsx", 
-#                        "ggplot2",
-#                        "Hmisc",
-#                        "xtable",
-#                        "ggthemes",
-#                        "scales",
-#                        "png", 
-#                        "jpeg",
-#                        "RColorBrewer")
-# 
-# 
-# pkgTest <- function(pkg){
-#   if(!require(pkg, character.only = TRUE)){
-#     install.packages(pkg, dep = TRUE)
-#     if(!require(pkg, character.only = TRUE)) stop("Package not found")
-#   }
-# }
-# # Test and install the list above
-# pkgTest(required.packages)
-# 
-# ## Capwords ------------------------------------------------------
-
 capwords <- function(s, strict = FALSE) {
   cap <- function(s) paste(toupper(substring(s, 1, 1)),
 {s <- substring(s, 2); if(strict) tolower(s) else s},
@@ -35,18 +12,22 @@ sep = "", collapse = " " )
 sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
 }
 
-# SA Slope calculator ----
-# Create a function that checks to see if there's enough data to calculate a linear regression, 
-# and then regresses pin height across time (as decimal year) 
-SAslopeer <- function(d) {
-  if(nrow(d) < 2) {return(data.frame(intercept = NA, slope = NA))
-  } else {
-    p <-  coef(lm(Raw ~ DecYear, data = d))
-    p <- data.frame(slope = round(p[2], digits= 4))
-  }
-}
 
 #*************************************************************
+# SA Slope calculator ---- slopeSAer
+# Create a function that checks to see if there's enough data to calculate a linear regression, 
+# and then regresses pin height across time (as decimal year) 
+slopeSAer <- function(e) {                                              
+	if(nrow(e) < 2) {return(data.frame(intercept = NA, slope = NA))     # if number of rows (data points) is less than 2 return NA's
+	} else {  # if there's enough data take data = e (which will be subsetted in later functions) then...
+		p <-  coef(lm(plug_mean ~ Dec_year, data = e))       # regress the plug depth against time (decimal years) and return the coefficients of the regression- slope and intercept
+		p <- data.frame(slope = round(p[2], digits= 4))      # subset out just the slope coefficient from the object p
+	}
+	
+} 
+
+#*************************************************************
+# SET Slope Calculator ---- slopeer based on Philippe Hensel's original code
 # Create a function that checks to see if there's enough data to calculate a linear regression, 
 # and then regresses pin height across time (as decimal year) 
 slopeer <- function(d) {
