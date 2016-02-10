ce2f9916a592c335841a14f67053da11665841d0
Adam Starke <afstarke@gmail.com>
2016-02-10 16:56:15 -0500

    initial commit

--- src/ElevationTrends_alternativeTechnique.R
+++ src/ElevationTrends_alternativeTechnique.R
@@ -5,6 +5,7 @@
 a <- SET.data.Melt %>% 
 	group_by(Position_ID, variable) %>% 
 	do(tidy(lm(Raw ~ DecYear, data = .))) %>% 
-	full_join(meanslope.Pin) %>% 
+	full_join(meanslope.Pin) %>% # replace with StudySites to bring in site names and other info.
 	mutate(diff = estimate - slope) %>% 
 	filter(term == "DecYear")
+
