b12b56d83c189c02bb33d204c8d5a0539d27e514
Adam Starke <afstarke@gmail.com>
2016-02-16 17:28:50 -0500

    change name of output

--- src/ElevationTrends_alternativeTechnique.R
+++ src/ElevationTrends_alternativeTechnique.R
@@ -11,10 +11,10 @@
 # Picks up from 'munge/Data_munge.R' to analyze 'cleaned' data
 #
 # user  system elapsed 
-# 5.99    0.03    6.04
+# 4.16    0.01    4.20 
 
 
-a <- SET.data.Melt %>% # Start with the munged and tidy dataframe (long format)
+a <- SET.data.compclean %>% # Start with the munged and tidy dataframe (long format)
 	group_by(Position_ID, Pin) %>% # Group by the full dataset by individual pin
 	do(tidy(lm(Raw ~ DecYear, data = .))) # %>% # apply a linear regression model of pin height against time (decimal year) 
 	full_join(meanslope.Pin) %>% # replace with StudySites to bring in site names and other info.
