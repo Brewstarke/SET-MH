55f968306002cf755f581bab9fe269afd12beea1
Adam Starke <afstarke@gmail.com>
2016-03-07 17:07:29 -0500

    fix

--- src/ElevationTrends_alternativeTechnique.R
+++ src/ElevationTrends_alternativeTechnique.R
@@ -19,19 +19,42 @@ SET_Summarize <- function(data){
 		group_by(Site_Name, Location_ID, Position_ID, pin_ID) %>% # Group by the full dataset by individual pin
 		do(tidy(lm(Raw ~ DecYear, data = .))) %>% # apply a linear regression model of pin height against time (decimal year) 
 		filter(term == 'DecYear') %>% 
-		group_by(Site_Name, Location_ID) %>% 
+		group_by(Site_Name, Location_ID, Position_ID) %>% 
 		summarise(meanElevationRate = mean(estimate), ElevationRate_se = stder(estimate)) %>% 
 		inner_join(StudyStations) %>% 
-		select(SET_ID, Site_Name, Stratafication, Plot_Name, SET_Type, meanElevationRate, ElevationRate_se)
+		select(SET_ID, Location_ID, Site_Name, Stratafication, Plot_Name, SET_Type, meanElevationRate, ElevationRate_se)
 	attr(df, which = "Datainfo") <- attr(data, "Datainfo")
 	df
 }
 
+# Run each filtered dataset through summarize funtion ----
 
-a <- SET_Summarize(SET.data.long)
+a <- SET_Summarize(SET.data.long) %>% mutate(dataset = "A")
 
-b <- SET_Summarize(SET.data.cleanV1)
+b <- SET_Summarize(SET.data.cleanV1) %>% mutate(dataset = "B")
 
-c <- SET_Summarize(SET.data.cleanV2)
+c <- SET_Summarize(SET.data.cleanV2) %>% mutate(dataset = "C")
 
-d <- SET_Summarize(SET.data.cleanV3)
+d <- SET_Summarize(SET.data.cleanV3) %>% mutate(dataset = "D")
+
+CompiledSET <- a %>% bind_rows(b) %>% bind_rows(c) %>% bind_rows(d)
+
+
+
+# Surface Accretion Summarization ----
+
+SA.rates  <- SA.data.long %>% 
+	rename(Location_ID = Location_ID.x) %>% 
+	select(Layer_ID, Location_ID, Date, Estab_Date, Accretion, Site_ID, Plot_Name, Organization, DecYear) %>% 
+	group_by(Layer_ID, Location_ID, Date, Estab_Date, Accretion, Site_ID, Plot_Name, Organization, DecYear) %>% 
+	summarise(plugMeanAcc = mean(Accretion), plugSEAcc = stder(Accretion)) %>% 
+	group_by(Layer_ID, Location_ID) %>% 
+	do(tidy(lm(plugMeanAcc ~ DecYear, data = .))) %>% 
+	filter(term == 'DecYear') %>%
+	left_join(StudyStations) %>% 
+	rename(Accretion = estimate, AccretionSE = std.error) %>% 
+	select(Layer_ID, Location_ID, Site_Name, Stratafication, Plot_Name, SET_Type, Accretion, AccretionSE)
+	
+
+
+
