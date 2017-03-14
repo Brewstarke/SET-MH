2fa39219136f57c889892e3ea9349a212385639b
Adam Starke <afstarke@gmail.com>
2017-03-14 10:59:28 -0400

    seperate packages into a single file to be shared across projects when needed.

---
+++ libraryLoad.R
@@ -0,0 +1,41 @@
+## @knitr Libraries
+
+# Data loading/munging
+library(readr)
+library(RODBC)
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+library(broom)
+library(magrittr)
+library(stringr)
+library(xlsx)
+library(xts)
+library(purrr)
+library(tidyquant)
+library(ggExtra)
+library(lubridate)
+
+# Graphics
+library(ggplot2)
+library(png)
+library(RColorBrewer)
+library(colorRamps)
+library(ggthemes)
+library(dygraphs)
+library(xtable)
+library(rmarkdown)
+library(tufte)
+library(tint)
+library(taucharts)
+
+
+# Spatial packages
+library(leaflet)
+library(rgdal)
+library(rgeos)
+library(sp)
+library(spatial)
+library(maptools)
+
+library(htmltools)
