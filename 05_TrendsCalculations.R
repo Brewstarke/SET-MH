ea0ddace1bd93209f2da7e3e396ca19a6f966095
Adam Starke <afstarke@gmail.com>
2017-04-03 17:36:34 -0400

    add lines to divide scripts, worked on Summary Tables for joining SET and MH trends.

--- 05_TrendsCalculations.R
+++ 05_TrendsCalculations.R
@@ -11,66 +11,43 @@
 # Picks up from '04_Data_munge.R' to calculate trends in elevation and accretion from 'tidy' data
 #
 
+#---------------------------------------------------------------------------------------------------------#
 
+# SET.station.Summary ----
+# The following code summarizes a linear model to each pin then summarizes (mean) to position, then station.
 
-SET_Summarize <- function(data){
-	
-	# SET_Summarize description ----
-	# SET_Summarize function takes a long format, rod-SET dataseries and calculates the 
-	# regression coefficient across all unique pin/direction/station/site locations
-	# then calculates the mean of those regression coefficients across directions and then stations
-	# to arrive at a 'Station level' summary statistic that can be used in further analysis
-	# as in '/src/TrendsAnalysis.R'
-	# Output is trimmed down to Site_Name and Location_ID
-	# SET_Summarize body ----
-	
-	df <- data %>% # Start with the munged and tidy dataframe (long format)
-		dplyr::group_by(Site_Name, SET_Type, Location_ID, Plot_Name, Position_ID, pin_ID) %>% # Group by the full dataset by individual pin
-		dplyr::do(tidy(lm(Raw ~ DecYear, data = .))) %>% # apply a linear regression model of pin height against time (decimal year) 
-		filter(term == 'DecYear')
-	SETpinLevel <- df # each pin has a linear model fit to it.
-		
-	df2 <- df %>% # Summarize down to position/direction level
-		ungroup() %>% 
-		group_by(Site_Name, SET_Type, Location_ID, Plot_Name, Position_ID) %>% 
-		dplyr::summarize(ElevationRate_mean = mean(estimate), ElevationRate_se = stder(estimate)) 
-	SETpositionLevel <- df2
-	
-	df3 <- df2 %>% 
-		ungroup() %>% # Summarize down to the station level.
-		group_by(Site_Name, SET_Type, Location_ID, Plot_Name) %>% 
-		dplyr::summarize(meanElevationRate = mean(ElevationRate_mean), ElevationRate_se = stder(ElevationRate_mean))
-		# select(SET_ID, Location_ID, Site_Name, Stratafication, Plot_Name, SET_Type, meanElevationRate, ElevationRate_se)
-	
-	
-	attr(df3, which = "Datainfo") <- attr(data, "Datainfo")
-	df3
-}
-
-
-
-# Using the more conservative approach- drop all pins that have had a mussel/hole issue during the readings. ----
-SET.station.Summary <- SET.data.long %>% 
+SET.station.Trends.summary <- SET.data.long %>% 
 	filter(bigIssuePin == FALSE) %>% # filter out the pins that had issues (for changes to this, see 04_Data_munge.R)
-	SET_Summarize() %>% 
-	mutate(dataset = "Any pins impacted by holes or mussels dropped")# "Any individual pin reading taken atop a hole or mussle has been dropped"
+	dplyr::group_by(Site_Name, SET_Type, Location_ID, Plot_Name, Position_ID, pin_ID) %>% # Group by the full dataset by individual pin
+	dplyr::do(tidy(lm(Raw ~ DecYear, data = .))) %>% # apply a linear regression model of pin height against time (decimal year) 
+	filter(term == 'DecYear') %>% 
+	ungroup() %>% 
+	group_by(Site_Name, SET_Type, Location_ID, Plot_Name, Position_ID) %>% 
+	dplyr::summarize(ElevationRate_mean = mean(estimate), ElevationRate_se = stder(estimate)) %>% 
+	ungroup() %>% # Summarize down to the station level.
+	group_by(Site_Name, SET_Type, Location_ID, Plot_Name) %>% 
+	dplyr::summarize(meanElevationRate = mean(ElevationRate_mean), ElevationRate_se = stder(ElevationRate_mean)) %>% 
+	mutate(dataset = "Any pins impacted by holes or mussels dropped") %>% # "Any individual pin reading taken atop a hole or mussle has been dropped"
+	`attr<-`("Date of analysis", Sys.Date())
 
+#---------------------------------------------------------------------------------------------------------#
 
 # SET-MH study site level summaries ----
+# Continues the summarizing method (mean) down to the Site level.
+SET.site.Trends.summary <-  SET.station.Trends.summary %>%
+	left_join(StudyStationLocations, by = c('Location_ID', 'Site_Name', 'SET_Type')) %>%
+	ungroup() %>% 
+	group_by(Site_Name, Stratafication, SET_Type) %>%
+	summarise(mean_ElevationRate = mean(meanElevationRate), ElevationRate_se = stder(meanElevationRate)) %>% 
+	`attr<-`("Date of analysis", Sys.Date())
 
-SET_site_Summary <- function(stationLeveldata){
-	stationLeveldata %>% left_join(StudyStationLocations, by = c('Location_ID', 'Site_Name', 'SET_Type')) %>% 
-		ungroup() %>% group_by(Site_Name, Stratafication, SET_Type) %>%
-		summarise(mean_ElevationRate = mean(ElevationRate_mean), ElevationRate_se = stder(ElevationRate_mean))
-}
-
-# Generate Site summary ----
-SET.site.Summary <- SET_site_Summary(SET.station.Summary)
+#---------------------------------------------------------------------------------------------------------#
 
 # Surface Accretion Rate Summarizations ----
 
 
 SA.rates  <- SA.data.long %>% 
+	filter(Date != '2008-08-08') %>% 
 	rename(Location_ID = Location_ID.x) %>% # rename ID field to fix join output
 	select(Layer_ID, Location_ID, Date, Estab_Date, Accretion, Site_ID, Plot_Name, Organization, DecYear) %>% # Select the columns needed
 	group_by(Layer_ID, Location_ID, Site_ID, Plot_Name, Organization) %>% # group down to the plug level
@@ -84,7 +61,8 @@ SA.rates  <- SA.data.long %>%
 	select(Layer_ID, Location_ID, Site_Name, Stratafication, Plot_Name, SurfaceAccretion_plot, SurfaceAccretionSE_plot) %>% 
 	ungroup() %>% 
 	group_by(Site_Name, Stratafication, Plot_Name, Location_ID) %>% 
-	summarise(StationMeanAcc = mean(SurfaceAccretion_plot), StationAccSE = stder(SurfaceAccretion_plot))
+	summarise(StationMeanAcc = mean(SurfaceAccretion_plot), StationAccSE = stder(SurfaceAccretion_plot)) %>% 
+	`attr<-`("Date of analysis", Sys.Date())
 		
 		
 	
